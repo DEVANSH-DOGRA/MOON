abf723e3b3abcc2cadf7ac2a7427cbb90f9eb9c0
DakshMalik02 <daksh0399.be21@chitkara.edu.in>
2022-05-31 22:28:59 +0530

    Code is appended and updated

--- main.cpp
+++ main.cpp
@@ -15,8 +15,22 @@ int main() {
 
     //  print array elements
     for (int n = 0; n < 5; ++n) {
-        if(numbers[n]%2!=0)
+        if(numbers[n]%2!=0){
         cout << numbers[n] << "  ";
+        }
+    
+    }
+
+    // I am also adding the sum and product of the numbers (Edited by Daksh)
+    int sum=0;
+    int product = 1;
+    for (int n = 0; n < 5; ++n) {
+        if(numbers[n]%2!=0){
+            sum+=numbers[n];
+            product*=numbers[n];
+            cout<<"Sum of the number is :"<<sum<<endl;
+            cout<<"Product of the number is :"<<product<<endl;
+        }
     }
 
     return 0;
